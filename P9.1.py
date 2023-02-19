3170049e678c6c27e6d260affbbd9fad051cca2d
h4cK3rM4n04 <eliemanoarm@gmail.com>
2023-02-19 21:41:39 +0100

    Crepie_19_02_23

--- P9.1.py
+++ P9.1.py
@@ -2,7 +2,7 @@ lst_1 = [1, 2, 3, 4]
 lst_2 = [1, 2, 3, 4, 5]
 lst_3 = [1, 6, 2, 3, 4, 5]
 
-def renverse(T):#Code by h4ck3rM4n°4
+def renverse(T):#Code by h4ck3rM4n°4 valide
 	return T[::-1]
 
 def r(T):#Code by ChatGPT................................................................................................
@@ -11,7 +11,7 @@ def r(T):#Code by ChatGPT.......................................................
         T[i], T[n-1-i] = T[n-1-i], T[i]
     return T
 
-def renverse(T):#Code by h4ck3rM4n°4
+def renverse(T):#Code by h4ck3rM4n°4 non valide par Mr
 	cpt = -1
 	x = []
 	for i in range(len(T)):
@@ -19,9 +19,11 @@ def renverse(T):#Code by h4ck3rM4n°4
 		cpt -= 1
 	return x
 
-def indice_maximum(T):#Code by h4ck3rM4n°4
+def somme_d_une_liste(T):#Code by h4ck3rM4n°4
 	cpt = 0
 	nbr_max = 0
-	for j in range(len(T)):
-		pass
-print(indice_maximum(lst_3))
+	nbr = 0
+	for j in T:
+		nbr += j
+	return nbr
+print(somme_d_une_liste(lst_3))
