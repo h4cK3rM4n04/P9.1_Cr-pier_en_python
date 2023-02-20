61eda89347703c5b831cfa1a28221351930b7005
h4cK3rM4n04 <eliemanoarm@gmail.com>
2023-02-20 20:38:40 +0100

    Crépier_NSI

--- P9.1.py
+++ P9.1.py
@@ -1,9 +1,11 @@
 lst_1 = [1, 2, 3, 4]
 lst_2 = [1, 2, 3, 4, 5]
 lst_3 = [1, 6, 2, 3, 4, 5]
+lst_4 = [10, 5, 7, 12, 9, 2, 1]
+lst_5 = [4, 9, 1, 6, 8, 3, 7, 2, 5]
 
-def renverse(T):#Code by h4ck3rM4n°4 valide
-	return T[::-1]
+def renverse(G):#Code by h4ck3rM4n°4 valide
+	return G[::-1]
 
 def r(T):#Code by ChatGPT................................................................................................
     n = len(T)
@@ -11,19 +13,24 @@ def r(T):#Code by ChatGPT.......................................................
         T[i], T[n-1-i] = T[n-1-i], T[i]
     return T
 
-def renverse(T):#Code by h4ck3rM4n°4 non valide par Mr
+def renverse(C):#Code by h4ck3rM4n°4 non valide par Mr
 	cpt = -1
 	x = []
-	for i in range(len(T)):
-		x.append(T[cpt])
+	for i in range(len(C)):
+		x.append(C[cpt])
 		cpt -= 1
 	return x
 
-def somme_d_une_liste(T):#Code by h4ck3rM4n°4
-	cpt = 0
-	nbr_max = 0
+def somme_d_une_liste(H):#Code by h4ck3rM4n°4
 	nbr = 0
-	for j in T:
+	for j in H:
 		nbr += j
 	return nbr
-print(somme_d_une_liste(lst_3))
+
+def indice_maximum(J):#Code by h4cK3rM4n°4
+	max_value = J[0]
+	for x in range(len(J)):
+		while max_value < J[x]:
+			max_value = J[x]
+	return max_value
+print(indice_maximum(lst_5))
