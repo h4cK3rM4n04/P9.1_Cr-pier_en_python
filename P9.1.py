fffa21b415f87a16084af95b4fa3f5e14e5d18b9
h4cK3rM4n04 <eliemanoarm@gmail.com>
2023-02-21 21:33:13 +0100

    P9.1 en cours de finalisation

--- P9.1.py
+++ P9.1.py
@@ -1,8 +1,12 @@
+import random
+
 lst_1 = [1, 2, 3, 4]
 lst_2 = [1, 2, 3, 4, 5]
 lst_3 = [1, 6, 2, 3, 4, 5]
 lst_4 = [10, 5, 7, 12, 9, 2, 1]
 lst_5 = [4, 9, 1, 6, 8, 3, 7, 2, 5]
+lst_6 = [11, 7, 13, 12, 14, 16, 19, 18, 9, 3, 5, 17, 1, 20, 10, 8, 6, 4, 2, 15]
+lst_7 = [2, 1, 3]
 
 def renverse(G):#Code by h4ck3rM4n°4 valide
 	return G[::-1]
@@ -27,15 +31,46 @@ def somme_d_une_liste(H):#Code by h4ck3rM4n°4
 		nbr += j
 	return nbr
 
-def maximum(J):#Code by h4cK3rM4n°4	#
+#=========Code by h4cK3rM4n°4=======#
+def maximum(J):						#
 	max_value = J[0]				#
 	for x in range(len(J)):			#
 		while max_value < J[x]:		#
 			max_value = J[x]		#
 	return max_value				#
 									#	Les deux fonctions ont pour but de trouver l'indice du maximum dans une liste!
-def indice_maximum(K):
-	for i in range(len(K)):
-		if K[i] == maximum(K):
-			return i
-print(indice_maximum(lst_4))
+def indice_maximum(K):				#
+	for i in range(len(K)):			#
+		if K[i] == maximum(K):		#
+			return i  				#
+#===================================#
+
+def tas_de_crepes(n):#Code by h4cK3rM4n04
+	v = [x for x in range(1, n+1)]
+	return v
+
+def melange_crepier(T):#Code in JupyterHub
+	random.shuffle(T)
+	return T
+
+
+def max_indice_0(T):
+    v = [x for x in renverse(T)]
+    lv = []
+    if v[0] != maximum(v):
+        lv = v[0]
+        b = indice_maximum(v)
+        v[0] = maximum(v)
+        v[b] = lv
+    return v
+
+def crepier(liste):#Code by ChatGPT................................................................................................
+    n = len(liste)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if liste[j] < liste[j+1]:
+                liste[j], liste[j+1] = liste[j+1], liste[j]
+    return liste
+
+def crepier(T):#Code by h4cK3rM4n04
+	pass
